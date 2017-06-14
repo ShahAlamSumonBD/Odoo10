5b1180cdeb16dc63b75ee9a53ed2b828248598fa
ShahAlamSumonGenweb2 <alam.shah@genweb2.com>
2017-06-14 15:31:33 +0600

    Stock indent Report with 3 Parameters done

--- odoo/custom_addons/stock_indent/reports/indent_report.py
+++ odoo/custom_addons/stock_indent/reports/indent_report.py
@@ -7,24 +7,24 @@ class GetStockIndentReport(models.AbstractModel):
     @api.model
     def render_html(self, docids, data=None):
         indent_pool = self.env['indent.indent'].search([])
-        indent_ids = indent_pool.search([('operating_unit_id','=',data['operating_unit_id']),('source_department_id','=',data['source_department_id'])])
+        all_indent_ids = indent_pool.search([('operating_unit_id','=',data['operating_unit_id']),
+                                         ('source_department_id','=',data['source_department_id'])
+                                         ])
+        operating_unit_wise_indent_ids = indent_pool.search([('operating_unit_id', '=', data['operating_unit_id'])])
 
-        # res=[]
-        # for line_pool in indent_pool.browse(indent_ids.ids):
-        #     for line in line_pool.product_lines:
-        #         r={}
-        #         r['name']=line.name
-        #         r['required_date']=line.required_date
-        #         r['product_uom_qty']=line.product_uom_qty
-        #         r['price_unit']=line.price_unit
-        #         r['price_subtotal']=line.price_subtotal
-        #
-        #         if r not in res:
-        #             res.append(r)
+        indent_ids=[]
+        if all_indent_ids:
+            for i in all_indent_ids:
+                indent_ids.append(i)
+        elif operating_unit_wise_indent_ids:
+            for i in operating_unit_wise_indent_ids:
+                indent_ids.append(i)
+
 
         docargs = {
             'operating_unit_name': data['operating_unit_name'],
             'source_department_name': data['source_department_name'],
+
             'docs': indent_ids,
         }
 
--- odoo/custom_addons/stock_indent/wizard/indent_report_wizard.py
+++ odoo/custom_addons/stock_indent/wizard/indent_report_wizard.py
@@ -5,8 +5,16 @@ class stock_indent_report(orm.TransientModel):
     _name='stock.indent.report.wizard'
 
     # required_date = fields.Date('Required Date')
-    operating_unit_id = fields.Many2one('operating.unit','Operating Unit')
-    source_department_id = fields.Many2one('stock.location', 'From Department')
+
+    operating_unit_id = fields.Many2one('operating.unit','Select Operating Unit',
+                                        required='True',
+                                        default = lambda self:self.env['res.users'].
+                                            operating_unit_default_get(self._uid)
+                                        )
+    source_department_id = fields.Many2one('stock.location', 'Select Department Location')
+
+    product_category_id = fields.Many2one('product.category', 'Select Product Category')
+    product_id = fields.Many2one('product.product', 'Select Product')
 
 
     @api.multi
@@ -17,4 +25,6 @@ class stock_indent_report(orm.TransientModel):
         data['operating_unit_name'] = self.operating_unit_id.name
         data['source_department_id'] = self.source_department_id.id
         data['source_department_name'] = self.source_department_id.name
+
+        data['product_id'] = self.product_id.id
         return self.env['report'].get_action(self, 'stock_indent.report_stock_indent_document', data=data)
